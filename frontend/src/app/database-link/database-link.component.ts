372f6a5e0efaf1f4584d66c169a7f3af969bdb42
Lukas <lukas.bromig@tum.de>
2020-11-24 18:21:04 +0100

    clean up database-link. Add functionality.
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- frontend/src/app/database-link/database-link.component.ts
+++ frontend/src/app/database-link/database-link.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Device, DeviceService, DeviceType } from '../device.service';
-import { Database, DatabaseService } from '../database.service';
+import { Device } from '../device.service';
+import { Database, DatabaseDeviceLink, DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-database-link',
@@ -10,57 +10,19 @@ import { Database, DatabaseService } from '../database.service';
 })
 
 export class DatabaseLinkComponent implements OnInit {
-    deviceTypes = [
-        {
-            value: DeviceType.SILA,
-            name: this.deviceService.deviceTypeAsName(DeviceType.SILA),
-        },
-        {
-            value: DeviceType.CUSTOM,
-            name: this.deviceService.deviceTypeAsName(DeviceType.CUSTOM),
-        },
-        {
-            value: DeviceType.SOFT,
-            name: this.deviceService.deviceTypeAsName(DeviceType.SOFT),
-        },
-    ];
-    deviceWorkCopy: Device;
-    databaseWorkCopy: Database;
-    databaseListWorkCopy: Database[];
-  constructor(public deviceService: DeviceService,
-              public databaseService: DatabaseService,
+    databaseListWorkCopy: Database[] = [];
+    databaseDeviceLink: DatabaseDeviceLink;
+  constructor(public databaseService: DatabaseService,
               public dialogRef: MatDialogRef<DatabaseLinkComponent>,
               @Inject(MAT_DIALOG_DATA) public device: Device,
               @Inject(MAT_DIALOG_DATA) public database: Database){
-              // Try this for the databaseList!!!!@Inject(MAT_DIALOG_DATA) public database: Database) {
-      /*
-      this.databaseWorkCopy = {
-          name: database.name,
-          address: database.address,
-          port: database.port,
-          online: database.online,
-          status: database.status,
-      };
-      */
-
-      this.deviceWorkCopy = {
-          // id: device.id,
-          uuid: device.uuid,
-          name: device.name,
-          type: device.type,
-          address: device.address,
-          port: device.port,
-          available: device.available,
-          user: device.user,
-          database: device.database,
+      this.databaseDeviceLink = {
+          deviceId: device.uuid,
+          databaseId: database.id,
       };
   }
-    async getDatabases() {
-        const databaseListWorkCopy = this.databaseService.getDatabaseList();
-        return databaseListWorkCopy;
-  }
-  ngOnInit(): void {
-      // this.databaseListWorkCopy = this.getDatabases();
+  async ngOnInit() {
+      this.databaseListWorkCopy = await this.databaseService.getDatabases();
   }
 
 }
