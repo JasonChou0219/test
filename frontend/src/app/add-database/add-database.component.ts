cc3123b70e0296d19831107e6d0f03f412c77f0a
Lukas <lukas.bromig@tum.de>
2020-11-24 09:06:39 +0100

    Add add-database component
    Signed-off-by: Lukas <lukas.bromig@tum.de>

---
+++ frontend/src/app/add-database/add-database.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
+import { Database, DatabaseService } from '../database.service';
+
+@Component({
+  selector: 'app-add-database',
+  templateUrl: './add-database.component.html',
+  styleUrls: ['./add-database.component.scss']
+})
+export class AddDatabaseComponent implements OnInit {
+    databaseInfo: Database;
+    constructor(
+        public dialogRef: MatDialogRef<AddDatabaseComponent>,
+        @Inject(MAT_DIALOG_DATA) public database: Database,
+    ) {
+        this.databaseInfo = {
+            name: 'InfluxDB',
+            address: '127.0.0.1',
+            port: 8060,
+            online: false,
+            status: 'disconnected',
+        };
+    }
+    ngOnInit(): void {
+    }
+
+}
