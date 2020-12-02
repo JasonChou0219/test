654eec8fc0c8c54e4b5d3969825f270c760a2ac7
Lukas <lukas.bromig@tum.de>
2020-12-02 19:48:44 +0100

    Link run button in commands to callCommand function (html). Add placeholder function that calls callFeatureCommand in deviceService
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- frontend/src/app/device-command/device-command.component.ts
+++ frontend/src/app/device-command/device-command.component.ts
@@ -5,7 +5,7 @@ import {
     OnChanges,
     SimpleChanges,
 } from '@angular/core';
-import { DeviceCommand } from '../device.service';
+import {DeviceCommand, DeviceService} from '../device.service';
 
 @Component({
     selector: 'app-device-command',
@@ -22,7 +22,7 @@ export class DeviceCommandComponent implements OnInit, OnChanges {
     paramValues: string[] = [];
     expand = false;
 
-    constructor() {}
+    constructor(private deviceService: DeviceService) {}
 
     ngOnChanges(changes: SimpleChanges) {
         for (const param of this.command.parameters) {
@@ -31,4 +31,15 @@ export class DeviceCommandComponent implements OnInit, OnChanges {
     }
 
     ngOnInit(): void {}
+
+    callCommand(name: string) {
+        console.log(
+            // this.deviceService.callFeatureCommand(
+            //    this.deviceUUID,
+            //    this.featureIdentifier,
+            //    name
+            //    map of this command.parameter.identifier and respective this.paramValues
+            // )
+        );
+    }
 }
