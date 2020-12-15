fd1e109da4e23169cc1c338f0ec26bfda9db8514
Lukas <lukas.bromig@tum.de>
2020-12-15 22:13:37 +0100

    Add new class proposals for DatabaseInfo and DatabaseStatus
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- source/device_manager/device_layer/database_info.py
+++ source/device_manager/device_layer/database_info.py
@@ -9,6 +9,25 @@ class DatabaseInfo:
     port: int
 
 @dataclass
+class DatabaseInfoNew:
+    id: int
+    name: str
+    address: str
+    port: int
+    username: str
+    password: str
+
+
+@dataclass
 class DatabaseStatus:
     online: bool
     status: str
+
+# Todo: Use the new DatabaseStatus class
+@dataclass
+class DatabaseStatusNew:
+    online: bool
+    retention_policy: str
+    error: str
+    ping: float
+    version: str
