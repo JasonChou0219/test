b39260011f455d5955eedb7adfa1b18c1a5f61be
David Leiter <davidleiter92@gmail.com>
2021-01-18 10:59:28 +0100

    Move dbconfig to thread local variable

--- source/device_manager/database.py
+++ source/device_manager/database.py
@@ -1,6 +1,7 @@
 import psycopg2
 import aioredis
 from os import path
+import configparser
 
 from .data_directories import DATA_DIRECTORY
 from .thread_local_storage import get_storage
@@ -18,9 +19,13 @@ async def get_redis_pool():
 def get_database_connection():
     storage = get_storage()
     if storage.get('conn') is None:
-        config = get_global_storage().get('config')['Database']
-        storage['conn'] = psycopg2.connect(host=config['host'],
-                                           port=config['port'],
-                                           user=config['user'],
-                                           password=config['password'])
+        if storage.get('dbconf') is None:
+            config = configparser.ConfigParser()
+            config.read(f'{DATA_DIRECTORY}/device-manager.conf')
+            storage['dbconf'] = config['Database']
+        dbconf = storage['dbconf']
+        storage['conn'] = psycopg2.connect(host=dbconf['host'],
+                                           port=dbconf['port'],
+                                           user=dbconf['user'],
+                                           password=dbconf['password'])
     return storage['conn']
