036db64a24bf01650d467e5683ac5484656ac91a
Lukas <lukas.bromig@tum.de>
2021-01-27 15:32:30 +0100

    Another readthedocs rubbish commit
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,3 +87,31 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# Add the generate_config.py script here so it can be executed in the build env of readthedocs
+
+#!/usr/bin/env python3
+import secrets
+import base64
+import configparser
+import os
+from source.device_manager.data_directories import DATA_DIRECTORY
+
+DIRECTORY = DATA_DIRECTORY
+CONFIG_FILE = f'{DIRECTORY}/device-manager.conf'
+
+config = configparser.ConfigParser()
+config['Security'] = {
+    'SecretKey': base64.b64encode(secrets.token_bytes(64)).decode()
+}
+config['Database'] = {
+    'host': 'localhost',
+    'port': 5432,
+    'user': 'postgres',
+    'password': '1234'
+}
+
+os.makedirs(DIRECTORY, exist_ok=True)
+with open(CONFIG_FILE, 'w') as configfile:
+    config.write(configfile)
