8d22433e0dcbeb015e309ee3d6d22a756029abe9
Lukas Bromig <lukas.bromig@tum.de>
2021-01-29 18:00:03 +0100

    trash commit 99999
    Signed-off-by: Lukas Bromig <lukas.bromig@tum.de>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -93,7 +93,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'images', '_images']
 
 
 # Add the generate_config.py script here so it can be executed in the build env of readthedocs
