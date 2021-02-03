8c38fa450a8cb2c79f092a02f21ff89f94da8841
Lukas <lukas.bromig@tum.de>
2021-02-03 11:32:25 +0100

    Add device-example code
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- examples/device_example.py
+++ examples/device_example.py
@@ -1,16 +1,32 @@
-import devices
-
 # WIP
 # This example will show you how to import a device client
 # This is work in progress
 
+import time
+
+
+def run(devices):
+    """ Instantiates selected devices for this experiment """
+
+    # Check which devices are available
+    print(f'{time.time()}: Imported devices: {[device.name for device in devices]}', flush=True)
+
+    # Assign device
+    client = devices[0]
+    print(f'Client is: {client} and of type {type(client)}', flush=True)
+
+    # Make a property call
+    response = client.call_property("SiLAService", "ServerName")
+    # response = devices[0].call_property("SiLAService", "ServerName")
+    print(f'Response is: {response}', flush=True)
+
 # ...
-yourObject = devices['HelloSiLA_server']()
+# yourObject = devices['HelloSiLA_server']()
 # You can call functions as described for every command and property in the device feature explorer under "Usage"
 # To call the property "StartYear" of the HelloSiLA example device use:
-StartYear = yourObject.call_property("GreetingProvider","StartYear")
+# StartYear = yourObject.call_property("GreetingProvider","StartYear")
 
 # To run the "SayHello" command use:
-response = yourObject.call_command("GreetingProvider","SayHello",parameters={"Name": 'some name'})
+# response = yourObject.call_command("GreetingProvider","SayHello",parameters={"Name": 'some name'})
 
 # Note: you need to replace the "yourObject" part of the command with the client object of that device!
