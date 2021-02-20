aeb0654ac74403922620164b3bde5adaab978575
david <david.leiter@tum.de>
2021-02-20 21:06:16 +0100

    Use supervisor for process management

--- backend.sh
+++ backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export DEVICE_MANAGER_ENV_PRODUCTION=1
 source .venv/bin/activate
-gunicorn --workers=1 --error-logfile /var/log/device-manager/device-manager.log -k uvicorn.workers.UvicornWorker backend:app
-#uvicorn --host=127.0.0.1 --port=8000 backend:app
+#gunicorn --workers=1 --error-logfile /var/log/device-manager/device-manager.log -k uvicorn.workers.UvicornWorker backend:app
+uvicorn --host=127.0.0.1 --port=8000 backend:app
