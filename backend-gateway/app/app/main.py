d10a3b0b6508efa0b0e7c3f7684e34d788c6de55
Lukas <lukas.bromig@tum.de>
2021-10-04 18:00:33 +0200

    Adjust environment variable for backend gateway
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- backend-gateway/app/app/main.py
+++ backend-gateway/app/app/main.py
@@ -19,6 +19,3 @@ if settings.BACKEND_CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
-
-from app.service_manager import auto_discovery
-print(auto_discovery.find())
