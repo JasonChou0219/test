9be8739cd37d88ae0fdc94173838eb265f6d2f19
Unknown <mail@rzechlin.de>
2021-10-12 17:48:08 +0200

    Copy backend-gateway as basis for wf-scheduler

---
+++ workflow-scheduler/app/app/schemas/__init__.py
@@ -0,0 +1,5 @@
+from .service import Service, ServiceCreate, ServiceInDB, ServiceUpdate
+from .item import Item, ItemCreate, ItemInDB, ItemUpdate
+from .msg import Msg
+from .token import Token, TokenPayload
+from .user import User, UserCreate, UserInDB, UserUpdate
