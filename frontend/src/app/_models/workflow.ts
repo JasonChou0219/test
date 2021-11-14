d21e1674fbe35c56802f53e527353ca6be4ba063
Lukas <lukas.bromig@tum.de>
2021-11-15 00:25:32 +0100

    Add models for python workflow and SiLA service
    Signed-off-by: Lukas <lukas.bromig@tum.de>

---
+++ frontend/src/app/_models/workflow.ts
@@ -0,0 +1,15 @@
+﻿export interface Workflow {
+    id?: number;
+    name: string;
+    fileName: string;
+    data: string;
+}
+export interface WorkflowInfo {
+    id: number;
+    name: string;
+    fileName: string;
+}
+
+export interface WorkflowInfoList {
+    data: WorkflowInfo[];
+}
