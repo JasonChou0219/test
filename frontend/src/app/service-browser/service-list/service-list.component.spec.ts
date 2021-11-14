79226048f0f6353f6010e9520457cb397e07f8a9
Lukas <lukas.bromig@tum.de>
2021-11-15 00:23:23 +0100

    Add service-list component
    Signed-off-by: Lukas <lukas.bromig@tum.de>

---
+++ frontend/src/app/service-browser/service-list/service-list.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
+
+import { ServiceListComponent } from './device-list.component'
+
+describe('ServiceListComponent', () => {
+    let component: ServiceListComponent
+    let fixture: ComponentFixture<ServiceListComponent>
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [ServiceListComponent],
+        }).compileComponents()
+    }))
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ServiceListComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    })
+
+    it('should create', () => {
+        expect(component).toBeTruthy()
+    })
+})
