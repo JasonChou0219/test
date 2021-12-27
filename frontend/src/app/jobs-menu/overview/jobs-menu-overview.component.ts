e58bf32b74da16061a65818f603f505d5d40620b
Lukas <lukas.bromig@tum.de>
2021-12-27 14:22:06 +0100

    Add job menu overview basic layout
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- frontend/src/app/jobs-menu/overview/jobs-menu-overview.component.ts
+++ frontend/src/app/jobs-menu/overview/jobs-menu-overview.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface RowData {
+    job: string;
+    status: boolean;
+    detailsLoaded: boolean;
+}
+
+
 @Component({
   selector: 'app-jobs-menu-overview',
   templateUrl: './jobs-menu-overview.component.html',
   styleUrls: ['./jobs-menu-overview.component.scss']
 })
 export class JobsMenuOverviewComponent implements OnInit {
+    dataSource: RowData[] = [];
+    tableColumns = [
+        'name',
+        'online',
+        'edit',
+    ];
+    selected: number | null = null;
 
   constructor() { }
 
