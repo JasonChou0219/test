f4ac03b3f74bf109e14bcc46e8ca3f0a7c9fcbe4
Lukas <lukas.bromig@tum.de>
2021-12-27 22:45:21 +0100

    Add Job model to frontend
    Signed-off-by: Lukas <lukas.bromig@tum.de>

---
+++ frontend/src/app/_models/job.ts
@@ -0,0 +1,36 @@
+﻿import { Workflow, WorkflowInfo, WorkflowInfoList } from '@app/_models';
+
+
+export interface Job {
+    id?: number;
+    uuid: uuid
+    title: string;
+    description?: string;
+    owner?: string;
+    owner_id?: number;
+    execute_at: Date;
+    running: boolean;
+    workflow: Workflow;
+    workflow_type: string;
+    workflow_execute_at: Date
+    workflow_running: boolean;
+}
+
+export interface JobInfo {
+    id?: number;
+    uuid: uuid
+    title: string;
+    description?: string;
+    owner?: string;
+    owner_id?: number;
+    execute_at: Date
+    running: boolean;
+    workflow_name: string;
+    workflow_type: string;
+    workflow_execute_at: Date
+    workflow_running: boolean
+}
+
+export interface JobInfoList {
+    data: JobInfo[];
+}
