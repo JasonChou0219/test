822e18382600586fa94e6d5fb015b4b665f61560
Lukas <lukas.bromig@tum.de>
2021-12-27 14:21:34 +0100

    Fix routing and and add styles
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -55,7 +55,7 @@ const routes: Routes = [
             },
             {
                 path: 'overview',
-                component: JobsMenuCreateComponent
+                component: JobsMenuOverviewComponent
             },
             {
                 path: 'scheduler',
