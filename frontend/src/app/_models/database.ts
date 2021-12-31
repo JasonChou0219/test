93a5e24c054af7563644ba29a8e5fcacb025d9a1
Lukas Bromig <lukas.bromig@tum.de>
2021-12-31 11:49:32 +0100

    Add models for booking, data-protocol and database
    Signed-off-by: Lukas Bromig <lukas.bromig@tum.de>

--- frontend/src/app/_models/database.ts
+++ frontend/src/app/_models/database.ts
@@ -24,24 +24,3 @@ export interface DatabaseStatus {
     online: boolean;
     status: string;
 }
-
-
-
-export const mock_database_info = {
-            id: 1,
-            title: 'Mock Database 1',
-        }
-
-export const mock_database_info_list = [{
-            id: 1,
-            title: 'Mock Database 1',
-        },
-        {
-            id: 2,
-            title: 'Mock Database 2'
-        },
-        {
-            id:3,
-            title: 'Mock Database 3'
-        }
-        ];
