8a880866a3099a03fc9832cc2172f21b55a9e7c4
Lukas <lukas.bromig@tum.de>
2022-02-14 11:11:11 +0100

    WS: Update workflow endpoints
    Signed-off-by: Lukas <lukas.bromig@tum.de>

--- workflow-scheduler/app/app/api/api_v1/endpoints/workflows.py
+++ workflow-scheduler/app/app/api/api_v1/endpoints/workflows.py
@@ -6,16 +6,15 @@ from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
 from app.api import deps
-from app.api.deps import get_db_workflow_designer_node_red as get_db_node_red
-from app.api.deps import get_db_workflow_designer_python as get_db_python
+# from app.api.deps import get_db_workflow_designer_node_red as get_db_node_red
+# from app.api.deps import get_db_workflow_designer_python as get_db_python
 
 router = APIRouter()
 
 
 @router.get("/", response_model=List[schemas.Workflow])
 def read_workflows(
-    db_node_red: Session = Depends(get_db_node_red),
-    db_python: Session = Depends(get_db_node_red),
+    db: Session = Depends(get_db),
     skip: int = 0,
     limit: int = 100,
     current_user: models.User = Depends(deps.get_current_active_user),
@@ -24,46 +23,32 @@ def read_workflows(
     Retrieve flows.
     """
     if crud.user.is_superuser(current_user):
-        workflows_node_red = crud.workflow.get_multi(db_node_red, skip=skip, limit=limit)
-        workflows_python = crud.workflow.get_multi(db_python, skip=skip, limit=limit)
+        workflows = crud.workflow.get_multi(db, skip=skip, limit=limit)
     else:
-        workflows_node_red = crud.workflow.get_multi_by_owner(
-            db=db_node_red, owner_id=current_user.id, skip=skip, limit=limit
+        workflows = crud.workflow.get_multi_by_owner(
+            db=db, owner_id=current_user.id, skip=skip, limit=limit
         )
-        workflows_python = crud.workflow.get_multi_by_owner(
-            db=db_python, owner_id=current_user.id, skip=skip, limit=limit
-        )
-    workflows = dict(workflows_node_red, **workflows_python)
     return workflows
 
 
 @router.post("/", response_model=schemas.Workflow)
 def create_workflow(
     *,
-    db_node_red: Session = Depends(get_db_node_red),
-    db_python: Session = Depends(get_db_node_red),
+    db: Session = Depends(get_db),
     workflow_in: schemas.WorkflowCreate,
     current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
     Create new workflow.
     """
-    if workflow_in.workflow_type == 'node-red':
-        workflow = crud.workflow.create_with_owner(db=db_node_red, obj_in=workflow_in, owner_id=current_user.id)
-    elif workflow_in.workflow_type == 'python':
-        workflow = crud.workflow.create_with_owner(db=db_python, obj_in=workflow_in, owner_id=current_user.id)
-    else:
-        workflow = crud.workflow.create_with_owner(db=db_python, obj_in=workflow_in, owner_id=current_user.id)
-        pass
-        # Todo: Implement a reasonable default or raise an exception
+    workflow = crud.workflow.create_with_owner(db=db, obj_in=workflow_in, owner_id=current_user.id)
     return workflow
 
 
 @router.put("/{uuid}", response_model=schemas.Workflow)
 def update_workflow(
     *,
-    db_node_red: Session = Depends(get_db_node_red),
-    db_python: Session = Depends(get_db_node_red),
+    db: Session = Depends(get_db),
     uuid: UUID,
     workflow_in: schemas.WorkflowUpdate,
     current_user: models.User = Depends(deps.get_current_active_user),
@@ -71,16 +56,7 @@ def update_workflow(
     """
     Update a workflow.
     """
-    if workflow_in.workflow_type == 'node-red':
-        workflow = crud.workflow.get(db=db_node_red, uuid=uuid)
-        db = db_node_red
-    elif workflow_in.workflow_type == 'python':
-        workflow = crud.workflow.get(db=db_python, uuid=uuid)
-        db = db_python
-    else:
-        workflow = crud.workflow.get(db=db_python, uuid=uuid)
-        db = db_python
-        # Todo: Implement a reasonable default or raise an exception
+    workflow = crud.workflow.get(db=db, uuid=uuid)
 
     if not workflow:
         raise HTTPException(status_code=404, detail="Workflow not found")
@@ -93,8 +69,7 @@ def update_workflow(
 @router.get("/{workflow_type}_{uuid}", response_model=schemas.Workflow)
 def read_workflow(
     *,
-    db_node_red: Session = Depends(get_db_node_red),
-    db_python: Session = Depends(get_db_node_red),
+    db: Session = Depends(get_db),
     workflow_type: str,
     uuid: str,
     current_user: models.User = Depends(deps.get_current_active_user),
@@ -102,13 +77,8 @@ def read_workflow(
     """
     Get workflow by id.
     """
-    if workflow_type == 'node-red':
-        workflow = crud.workflow.get(db=db_node_red, uuid=uuid)
-    elif workflow_type == 'python':
-        workflow = crud.workflow.get(db=db_python, uuid=uuid)
-    else:
-        workflow = crud.workflow.get(db=db_python, uuid=uuid)
-        # Todo: Implement a reasonable default or raise an exception
+
+    workflow = crud.workflow.get(db=db, uuid=uuid)
     if not workflow:
         raise HTTPException(status_code=404, detail="Workflow not found")
     if not crud.user.is_superuser(current_user) and (workflow.owner_id != current_user.id):
@@ -119,8 +89,7 @@ def read_workflow(
 @router.delete("/{uuid}", response_model=schemas.Workflow)
 def delete_workflow(
     *,
-    db_node_red: Session = Depends(get_db_node_red),
-    db_python: Session = Depends(get_db_node_red),
+    db: Session = Depends(get_db),
     workflow_type: str,
     uuid: UUID,
     current_user: models.User = Depends(deps.get_current_active_user),
@@ -128,16 +97,7 @@ def delete_workflow(
     """
     Delete a workflow.
     """
-    if workflow_type == 'node-red':
-        workflow = crud.workflow.get(db=db_node_red, uuid=uuid)
-        db = db_node_red
-    elif workflow_type == 'python':
-        workflow = crud.workflow.get(db=db_python, uuid=uuid)
-        db = db_python
-    else:
-        workflow = crud.workflow.get(db=db_python, uuid=uuid)
-        db = db_python
-        # Todo: Implement a reasonable default or raise an exception
+    workflow = crud.workflow.get(db=db, uuid=uuid)
     if not workflow:
         raise HTTPException(status_code=404, detail="Workflow not found")
     if not crud.user.is_superuser(current_user) and (workflow.owner_id != current_user.id):
