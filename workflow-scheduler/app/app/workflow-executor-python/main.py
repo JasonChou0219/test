12576e54e0425d1c0ac9e34a4c230f0fc01a7624
Lukas Bromig <lukas.bromig@tum.de>
2022-03-10 16:15:43 +0100

    WEP: Add reglo_lib file to image. Add logger and log folder to image. Add logger to main
    Signed-off-by: Lukas Bromig <lukas.bromig@tum.de>

--- workflow-scheduler/app/app/workflow-executor-python/main.py
+++ workflow-scheduler/app/app/workflow-executor-python/main.py
@@ -1,6 +1,35 @@
+import logging
+from datetime import datetime
+from uuid import UUID
+
 import workflow
 import services
-from uuid import UUID
+
+
+
+# Function to initialize the logging, logs a) to console and b) to file, please note that the file logging will only be
+# appended to one single file
+def initialize_logging():
+    # Initialize logging
+    logger_object = logging.getLogger(__name__)
+    logger_object.setLevel(logging.DEBUG)
+    logger_object.propagate = False
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # File handler
+    ts = datetime.now()
+    filehandler = logging.FileHandler(f'logs/{ts.year}_{ts.month}_{ts.day}_{ts.hour}_{ts.minute}_{ts.second:.0f}_main.log', mode='a')
+    filehandler.setLevel(logging.INFO)
+    filehandler.setFormatter(formatter)
+    logger_object.addHandler(filehandler)
+
+    # Console handler, i.e. output to command window
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+    logger_object.addHandler(console_handler)
+    return logger_object
 
 
 def main():
@@ -10,8 +39,13 @@ def main():
     #     for info in devices.devices
     # ]
     # return workflow_script.run(devs)
+    logger = initialize_logging()
+    logger.info(f'Logger name: {logging.getLogger(__name__)} \n')
+    logger.info('main.py executed \n')
+    print('Services are: ', services)
     print('main.py executed \n')
-    return workflow.run()
+    return workflow.run(services)
+
 
 if __name__ == "__main__":
     main()
