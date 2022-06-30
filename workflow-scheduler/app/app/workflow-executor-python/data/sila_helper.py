23c2480e994feed2c52165aa58dc62a1e51bf953
Lukas Bromig <lukas.bromig@tum.de>
2022-06-30 11:54:37 +0200

    WS: Add DASGIP class to sila_helper
    Signed-off-by: Lukas Bromig <lukas.bromig@tum.de>

--- workflow-scheduler/app/app/workflow-executor-python/data/sila_helper.py
+++ workflow-scheduler/app/app/workflow-executor-python/data/sila_helper.py
@@ -1,13 +1,11 @@
-import json
 from uuid import UUID
-
 from requests import post, get
 
 # Default settings
-target_service_hostname = "http://localhost"  # -> to env var
+target_service_hostname = "http://10.152,248.14"  # -> to env var
 target_service_port = '80'  # settings.BACKEND_GATEWAY_UVICORN_PORT  # -> to env var
 target_service_api_version = "/api/v1"  # settings.API_V1_STR  # -> to env var
-target_service_url = f'{target_service_hostname}:{target_service_port}{target_service_api_version}/'
+target_service_url = f'{target_service_hostname}{target_service_api_version}/'
 
 
 def _get_target_service_url():
@@ -31,7 +29,7 @@ def run_command(service_uuid: UUID, feature_identifier: str, function_identifier
                            'function_identifier': function_identifier,
                            'response_identifiers': response_identifier
                            },
-                   data=json.dumps(parameters)
+                   json=parameters
                    ).json()['response']
         return res
     except KeyError:
@@ -69,3 +67,51 @@ def instantiate_sila_client(address: str, port: int):
             continue
 
     return None
+
+
+class DASGIP:
+
+    def __init__(self, address: str, port: int, unit: int):
+        self.client = instantiate_sila_client(address=address, port=port)
+        self.uuid = self.client['uuid']
+        self.unit = unit
+
+    # Temperature control
+
+    def disable_temperature_control(self):
+        return run_command(service_uuid=self.uuid, feature_identifier='TemperatureServicer', function_identifier='SetCmd',
+                           parameters={"UnitID": self.unit, "Cmd": 1}, response_identifier="CmdSet")['CmdSet']
+
+    def enable_temperature_control(self):
+        return run_command(service_uuid=self.uuid, feature_identifier='PHServicer', function_identifier='SetCmd',
+                           parameters={"UnitID": self.unit, "Cmd": 2}, response_identifier="CmdSet")['CmdSet']
+    # pH control
+
+    def disable_pH_control(self):
+        return run_command(service_uuid=self.uuid, feature_identifier='PHServicer', function_identifier='SetCmd',
+                           parameters={"UnitID": self.unit, "Cmd": 1}, response_identifier="CmdSet")['CmdSet']
+
+    def enable_pH_control(self):
+        return run_command(service_uuid=self.uuid, feature_identifier='PHServicer', function_identifier='SetCmd',
+                           parameters={"UnitID": self.unit, "Cmd": 2}, response_identifier="CmdSet")['CmdSet']
+
+    # Stirrer control
+
+    def stop_stirrer(self):
+        return run_command(service_uuid=self.uuid, feature_identifier='AgitationServicer', function_identifier='SetCmd',
+                           parameters={"UnitID": self.unit, "Cmd": 1}, response_identifier="CmdSet")['CmdSet']
+
+    def start_stirrer(self):
+        return run_command(service_uuid=self.uuid, feature_identifier='AgitationServicer', function_identifier='SetCmd',
+                           parameters={"UnitID": self.unit, "Cmd": 2}, response_identifier="CmdSet")['CmdSet']
+    # Level sensor
+
+    def get_level_sensor(self):
+        return run_command(service_uuid=self.uuid, feature_identifier='LevelServicer', function_identifier='GetPV',
+                           parameters={"UnitID": self.unit}, response_identifier="CurrentPV")['CurrentPV']
+
+    # DO control
+
+    def get_DO_sensor(self):
+        return run_command(service_uuid=self.uuid, feature_identifier='DOServicer', function_identifier='GetPV',
+                           parameters={"UnitID": self.unit}, response_identifier="CurrentPV")['CurrentPV']
